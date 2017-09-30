3554bc5d334dfd2db48aa719c9fc0975a5c40e5a
dmike16 <cipmiky@gmail.com>
2017-10-01 01:13:11 +0200

    [ADD] Add webpack production config
    Description
    
    ===================================
        [OP]:
        -* ADD -> Add new stuff
        -* DEL -> Delete stuff
        -* MOD -> Modifies stuff
        -* FIX -> Fix Bugs
        -* MER -> Merge Branch
        -* MEF -> Merge fixing conflit
    
    ===================================

--- angular2/webpack/utils.bash
+++ angular2/webpack/utils.bash
@@ -75,7 +75,10 @@ module.exports = {
       //Html rule
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: {
+          loader: 'html-loader',
+          options:{minimize:false}
+        }
       },
       //Image and fonts rule
       {
@@ -122,7 +125,15 @@ module.exports = {
 COMMON
   return $?
 }
-
+# ============================================================================ #
+# Create developemnt configuration
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  cat exit status
+# ============================================================================ #
 function webpack__config_dev() {
   cat >${1}/webpack.dev.js <<DEV
 const webpackMerge = require('webpack-merge');
@@ -152,7 +163,59 @@ module.exports = webpackMerge(commonConfig,{
 DEV
   return $?
 }
+# ============================================================================ #
+# Create production configuration
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  cat exit status
+# ============================================================================ #
+function webpack__config_production() {
+  cat >${1}/webpack.prod.js <<PROD
+const webpackMerge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const commonConfig = require('./webpack.common.js');
+const helper = require('./helper');
 
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig,{
+  devtool: 'source-map',
+
+  output: {
+    path: helper.root('dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[id].[chunkhash].chunk.js'
+  },
+
+  plugins:[
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'boilerplate'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      uglifyOptions: {
+        mangle:{
+          keep_fnames: true
+        }
+      }
+    }),
+    new ExtractTextPlugin('[name].[chunkhash].css'),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'ENV': JSON.stringify(ENV)
+      }
+    })
+  ]
+});
+PROD
+}
 # ============================================================================ #
 # Create the package.json with param passed from
 # Globals:
@@ -226,7 +289,7 @@ import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
 
 if(process.env.ENV === 'production'){
-  //Prodaction
+  //Production
 }else{
   //Development
   Error['stackTraceLimit'] = Infinity;
