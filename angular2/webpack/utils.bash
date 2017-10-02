14ebec07678dcde91989ab238970c873e703ef3f
dmike16 <cipmiky@gmail.com>
2017-10-02 21:36:05 +0200

    [MOD] Use backtips in prodaction console.log
    Description
    
    ===================================
        [OP]:
        -* ADD -> Add new stuff
        -* DEL -> Delete stuff
        -* MOD -> Modifies stuff
        -* FIX -> Fix Bugs
        -* MER -> Merge Branch
        -* MEF -> Merge fixing conflit
    
    ===================================

--- angular2/webpack/utils.bash
+++ angular2/webpack/utils.bash
@@ -366,8 +366,8 @@ import { AppModule } from './app/app.module';
 
 if (process.env.ENV === 'production') {
   enableProdMode();
-  console.log('%c'+process.env.PROJECT_NAME,'font-size:13px;color:#0147A7;');
-  console.log('%cv'+process.env.VERSION,'font-size:11px;color:#c4342e;');
+  console.log(`%c${process.env.PROJECT_NAME}`,'font-size:13px;color:#0147A7;');
+  console.log(`%cv${process.env.VERSION}`,'font-size:11px;color:#c4342e;');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
