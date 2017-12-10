c784783fb59518e055551179250524442bffe717
dmike16 <cipmiky@gmail.com>
2017-12-10 11:45:37 +0100

    [FIX] Fix component logic generators
    Now we can generate a component with:
     angular_setup generate component [parent]/name
    and the component will be created inside [parent]/name
    with the class name in camel case
    
    ==================================
    <<<<<<<<< dmike16           <<<<<<
    <<<<<<<<< cipmiky@gmail.com <<<<<<
    ==================================

--- angular2/config/angularbashutil/generators/component_generator.bash
+++ angular2/config/angularbashutil/generators/component_generator.bash
@@ -9,17 +9,15 @@
 function generator_component() {
     logging__info "Generating component ${1}"
     local module
-    ## Check module name
-    if [[ $# -lt 3 ]]; then
-      module="src/app"
-    else
-      module="src/${2}"
-    fi
+    local component
+
+    component=${1##*/}
+    module=${2:-"app"}
     ## Check project initialized
     if [[ -d "./src" ]]; then
-      if [[ -d "${module}" ]]; then
-        __generator__create_component "${1}" "${module}" || return $?
-        logging__info "Add the component ${1} to the module ${module##*/}"
+      if [[ -d "src/${module}" ]]; then
+        __generator__create_component "${1}" "src/${module}" || return $?
+        logging__info "Add the component ${component} to the module ${module}"
       else
         logging__error "Project not initialized"
         return 1
@@ -32,25 +30,35 @@ function generator_component() {
 
 function __generator__create_component() {
   local conteiner
+  local module
+  local component
+
+  component=${1##*/}
+  module=${2##*/}
   container="${2}/${1}"
+  ## Not overwrite
+  if [[ -d "${container}" ]]; then
+    logging__error "Component already crated"
+    return 1
+  fi
   ## Create folder component
-  mkdir "${container}" || return $?
+  mkdir -p "${container}" || return $?
   ## Create html template
-  touch "${container}/${1}.component.html" || return $?
+  touch "${container}/${component}.component.html" || return $?
   ## Create stylesheet
-  touch "${container}/${1}.component.scss" || return $?
+  touch "${container}/${component}.component.scss" || return $?
   ## Create ts controller
   local ts_class
-  ts_class=$(printf ${1:0:1} | tr "[[:lower:]]" "[[:upper:]]")${1:1}
-  cat > "${container}/${1}.component.ts"<<CONTROLLER
+  ts_class=$(perl -pe 's/(^|-)(\w)/\U\2/g' <<< "${component}")
+  cat > "${container}/${component}.component.ts"<<CONTROLLER
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '${2##*/}-${1}',
-  templateUrl: './${1}.component.html',
-  styleUrls: ['./${1}.component.scss']
+  selector: '${2##*/}-${component}',
+  templateUrl: './${component}.component.html',
+  styleUrls: ['./${component}.component.scss']
 })
-export class ${ts_class} {
+export class ${ts_class}Component {
   constructor(){}
 }
 CONTROLLER
