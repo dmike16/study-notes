2eb2b558a22f37065051f2886269f4b3cc5eed52
dmike16 <cipmiky@gmail.com>
2020-02-08 20:05:25 +0100

    feat(quarkus): switch to gradle kts)

---
+++ quarkus/my-artifactId/build.gradle.kts
@@ -0,0 +1,32 @@
+val quarkusPluginVersion: String by project
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+plugins {
+    java
+    id ("io.quarkus") version "1.2.0.Final"
+}
+
+repositories {
+     mavenLocal()
+     mavenCentral()
+}
+
+dependencies {
+    implementation ("io.quarkus:quarkus-resteasy-jsonb")
+    implementation (enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation ("io.quarkus:quarkus-resteasy")
+
+    testImplementation ("io.quarkus:quarkus-junit5")
+    testImplementation ("io.rest-assured:rest-assured")
+}
+
+group = "dl.dual-lab"
+version = "1.0.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
