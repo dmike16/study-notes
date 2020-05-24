faa95d069fb949f45a7e46364297601b38fe8b6a
dmike16 <cipmiky@gmail.com>
2020-05-24 23:25:37 +0200

    vector bit set example

--- go/packs/chp06/main.go
+++ go/packs/chp06/main.go
@@ -15,4 +15,19 @@ func main() {
 	fmt.Println(triangle.Distance())
 	triangle = nil
 	fmt.Println(triangle.Distance())
+	
+	var x, y utils.IntSet
+	
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(42), y.Has(9))
 }
