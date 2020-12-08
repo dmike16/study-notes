9020395fafc31f8b28fa14364a28f704a01b7d68
dmike <cipmiky@gmail.com>
2020-12-08 17:17:46 +0100

    docs(ml): example of first classification

---
+++ machine-learning/intro/iris_classification.py
@@ -0,0 +1,39 @@
+from sklearn.datasets import load_iris
+from pandas import plotting as pd_plotting
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load datasets
+iris_dataset = load_iris()
+print("Iris keys: ", iris_dataset.keys())
+print("Target names: ", iris_dataset['target_names'])
+print("Feature_name: ", iris_dataset['feature_names'])
+print("Target: ", type(iris_dataset['target']))
+print("Data: ", type(iris_dataset['data']))
+print("Data shape: ", iris_dataset['data'].shape)
+
+# shufflte + split data in training_set + test_set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+print("Train data shape: ", np.array(X_train).shape)
+print("Train labels shap: ", np.array(y_train).shape)
+print("Test data shape: ", np.array(X_test).shape)
+print("Test label shape: ", np.array(y_test).shape)
+
+from matplotlib.colors import ListedColormap
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+cm2 = ListedColormap(['#0000aa', '#ff2020', '#50ff50'])
+pd_plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15),
+        marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=cm2)
+
+#plt.show()
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+print("k-nearest algo: ", knn)
+y_pred = knn.predict(X_test)
+print("Test set predition: ", y_pred)
+print("Score prediction: {:.2f}".format(np.mean(y_pred == y_test)))
+print("Score prediction: {:.2f}".format(knn.score(X_test, y_test)))
---
+++ machine-learning/intro/version.py
@@ -0,0 +1,20 @@
+import sys
+print("Python version: ", sys.version)
+
+import pandas as pd
+print("Pandas version: ", pd.__version__)
+
+import matplotlib
+print("Matplot version: ", matplotlib.__version__)
+
+import numpy as np
+print("Numpy version: ", np.__version__)
+
+import scipy as sp
+print("Scipy version: ", sp.__version__)
+
+import IPython
+print("IPython version: ", IPython.__version__)
+
+import sklearn
+print("scikit-learn version: ", sklearn.__version__)
