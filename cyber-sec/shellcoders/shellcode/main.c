5520082d0b2a323aa9cb9c66c49f5d8c2bc93dd0
dmike <cipmiky@gmail.com>
2021-03-28 03:07:44 +0200

    docs(shellcode): create and execute simple shellcode

---
+++ cyber-sec/shellcoders/shellcode/main.c
@@ -0,0 +1,18 @@
+#include <sys/mman.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+/**
+ * Compile with -fno-stack-protector -z execstack
+ */
+
+int main()
+{
+    unsigned char shellcodes[] = "\xb8\x01\x00\x00\x00"
+                                 "\xbb\x05\x00\x00\x00"
+                                 "\xcd\x80";
+    int (*ret)() = (int (*)())shellcodes;
+    (int)(*ret)();
+}
